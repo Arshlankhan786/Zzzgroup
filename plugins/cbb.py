376079202978edb5b50efacd6a5356f67ea9b21c
Pathan Arshlan <141414515+Arshlankhan786@users.noreply.github.com>
2024-09-07 12:07:40 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ : <a href='https://t.me/Pathans_movies'>Pᴀᴛʜᴀɴ Mᴏᴠɪᴇs </a>\n○ Sᴜᴘᴘᴏʀᴛ ᴄʜᴀɴɴᴇʟ : <a href='https://t.me/tokyorevengeandanime'>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ : <a href='t.me/zzmovie1000'>Mᴏᴠɪᴇs </a>\n○ Sᴜᴘᴘᴏʀᴛ : <a href='https://t.me/Forgemaster0'>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
